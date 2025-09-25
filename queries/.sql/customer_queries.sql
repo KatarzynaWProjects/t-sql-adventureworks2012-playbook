c217d4e0d0c05b82169c2d4be3b877e01e660717
Katarzyna Wróblewska <kaslox@o2.pl>
2025-09-25 23:40:47 +0200

    Update customer_queries.sql

--- queries/.sql/customer_queries.sql
+++ queries/.sql/customer_queries.sql
@@ -1,1 +1,6 @@
+/* ==========================================================
+   File: customer_queries.sql
+   DB  : AdventureWorks2012
+   Note: Daty filtrowane bez funkcji na kolumnie (sargable)
+   ========================================================== */
 
